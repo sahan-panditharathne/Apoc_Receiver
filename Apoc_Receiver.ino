5f7ea7bdb3405a2d0bfbd7ee2ca4704dfd6cdfd0
sahan-panditharathne <72350418+sahan-panditharathne@users.noreply.github.com>
2024-03-20 16:19:37 +0530

    Update Apoc_Receiver.ino

--- Apoc_Receiver.ino
+++ Apoc_Receiver.ino
@@ -1,9 +1,3 @@
-//This example code is in the Public Domain (or CC0 licensed, at your option.)
-//By Evandro Copercini - 2018
-//
-//This example creates a bridge between Serial and Classical Bluetooth (SPP)
-//and also demonstrate that SerialBT have the same functionalities of a normal Serial
-
 #include <SPI.h>
 #include "FS.h"
 #include "SD.h"
